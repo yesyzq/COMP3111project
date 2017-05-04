b1df366272b292e3ddc17f4a0a0261ff15dd00c4
Shangzhe WU <swuai@connect.ust.hk>
2017-05-04 17:40:05 +0800

    added display shipment

--- SinExWebApp20256461/ViewModels/InvoicesListViewModel.cs
+++ SinExWebApp20256461/ViewModels/InvoicesListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,19 @@ namespace SinExWebApp20256461.ViewModels
 {
     public class InvoicesListViewModel
     {
+        [Display(Name = "Waybill Number")]
         public virtual string WaybillNumber { get; set; }
+        [Display(Name = "Service Type")]
         public virtual string ServiceType { get; set; }
+        [Display(Name = "Shipped Date")]
         public virtual DateTime ShippedDate { get; set; }
+        [Display(Name = "Recipient Name")]
         public virtual string RecipientName { get; set; }
+        [Display(Name = "Total Amount Payable")]
         public virtual double TotalAmountPayable { get; set; }
         public virtual string Origin { get; set; }
         public virtual string Destination { get; set; }
+        [Display(Name = "Shipping Account Number")]
         public virtual string ShippingAccountNumber { get; set; }
         public virtual string Type { get; set; }
     }
--- SinExWebApp20256461/ViewModels/ShipmentsListViewModel.cs
+++ SinExWebApp20256461/ViewModels/ShipmentsListViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,14 +8,22 @@ namespace SinExWebApp20256461.ViewModels
 {
     public class ShipmentsListViewModel
     {
-        public virtual int WaybillId { get; set; }
+        //public virtual int WaybillId { get; set; }
+        [Display(Name = "Waybill Number")]
+        public virtual string WaybillNumber { get; set; }
+        [Display(Name = "Service Type")]
         public virtual string ServiceType { get; set; }
+        [Display(Name = "Shipped Date")]
         public virtual DateTime ShippedDate { get; set; }
+        [Display(Name = "Delivered Date")]
         public virtual DateTime DeliveredDate { get; set; }
+        [Display(Name = "Recipient Name")]
         public virtual string RecipientName { get; set; }
+        [Display(Name = "Number of Packages")]
         public virtual int NumberOfPackages { get; set; }
         public virtual string Origin { get; set; }
         public virtual string Destination { get; set; }
-        public virtual string ShippingAccountNumber{ get; set; }
+        [Display(Name = "Shipping Account Number")]
+        public virtual string ShippingAccountNumber { get; set; }
     }
 }

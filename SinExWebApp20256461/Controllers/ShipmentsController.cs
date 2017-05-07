827d7961fcf5e7dcca64eb63f8ecd9aebc4ed0b8
Shangzhe WU <elliottwu@users.noreply.github.com>
2017-05-07 20:49:44 +0800

    fixed bug in invoice report

--- SinExWebApp20256461/Controllers/ShipmentsController.cs
+++ SinExWebApp20256461/Controllers/ShipmentsController.cs
@@ -96,13 +96,15 @@ namespace SinExWebApp20256461.Controllers
             if (!string.IsNullOrWhiteSpace(ShippedStartDate))
             {
                 DateTime shippedStartDate = DateTime.ParseExact(ShippedStartDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-                shipmentQuery = shipmentQuery.Where(a => a.ShippedDate >= shippedStartDate);
+                shippedStartDate = shippedStartDate.AddDays(-1);
+                shipmentQuery = shipmentQuery.Where(a => a.ShippedDate > shippedStartDate);
             }
 
             if (!string.IsNullOrWhiteSpace(ShippedEndDate))
             {
                 DateTime shippedEndDate = DateTime.ParseExact(ShippedEndDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-                shipmentQuery = shipmentQuery.Where(a => a.ShippedDate <= shippedEndDate);
+                shippedEndDate = shippedEndDate.AddDays(1);
+                shipmentQuery = shipmentQuery.Where(a => a.ShippedDate < shippedEndDate);
             }
 
             ViewBag.ServiceTypeSortParm = sortOrder == "ServiceType" ? "ServiceType_desc" : "ServiceType";
@@ -229,13 +231,15 @@ namespace SinExWebApp20256461.Controllers
             if (!string.IsNullOrWhiteSpace(ShippedStartDate))
             {
                 DateTime shippedStartDate = DateTime.ParseExact(ShippedStartDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-                invoiceQuery = invoiceQuery.Where(a => a.ShippedDate >= shippedStartDate);
+                shippedStartDate = shippedStartDate.AddDays(-1);
+                invoiceQuery = invoiceQuery.Where(a => a.ShippedDate > shippedStartDate);
             }
 
             if (!string.IsNullOrWhiteSpace(ShippedEndDate))
             {
                 DateTime shippedEndDate = DateTime.ParseExact(ShippedEndDate, "yyyy-MM-dd", System.Globalization.CultureInfo.InvariantCulture);
-                invoiceQuery = invoiceQuery.Where(a => a.ShippedDate <= shippedEndDate);
+                shippedEndDate = shippedEndDate.AddDays(1);
+                invoiceQuery = invoiceQuery.Where(a => a.ShippedDate < shippedEndDate);
             }
 
             ViewBag.ServiceTypeSortParm = (sortOrder == "serviceType") ? "serviceType_desc" : "serviceType";

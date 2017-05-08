de78286139610bdfcb4714797a0e749099062a67
yesyzq <yesyzq@gmail.com>
2017-05-08 18:32:56 +0800

    allow multiple users with email addresses

--- SinExWebApp20256461/App_Start/IdentityConfig.cs
+++ SinExWebApp20256461/App_Start/IdentityConfig.cs
@@ -47,7 +47,7 @@ namespace SinExWebApp20256461
             manager.UserValidator = new UserValidator<ApplicationUser>(manager)
             {
                 AllowOnlyAlphanumericUserNames = true,
-                RequireUniqueEmail = true
+                RequireUniqueEmail = false
             };
 
             // Configure validation logic for passwords
